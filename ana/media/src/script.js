0b448c8b8dd68efde1d52e9dbd01d92160cbb667
aritarruda <aritarruda@hotmail.com>
2023-05-16 11:06:21 -0300

    adicionar pasta media

---
+++ ana/media/src/script.js
@@ -0,0 +1,49 @@
+/*const pagamento =180;
+
+if (pagamento >= 300){
+  console.log("voce recebeu o desconto de 30%")
+} else if (pagamento > 150){
+  console.log("desconto de 15%")
+}else {
+  console.log("voce nao recebeu desconto")
+}*/
+
+/*n1=10;
+n2=8;
+nota=(n1+n2)/2
+
+if( nota >7 ){
+  console.log("aprovado")
+}else{
+    console.log("reprovado")
+  }*/
+/*velocidade=20;
+if(velocidade >100){
+  console.log("multado")
+}else{
+  console.log("velocidade normal")
+}*/
+/*const a=5
+const b=10
+const soma=(a+b)
+console.log(`a soma vale: ${soma}`)
+
+let cor="azul ";
+let nome=" sua cor preferida";
+let mensagem=(cor + "," +nome)
+console.log(`a ${nome} é : ${cor}`)
+
+n="Ana"
+n1=10;
+n2=8;
+nota=(n1+n2)/2
+console.log(`a media de ${n} foi : ${nota}`)*/
+
+if(true) {
+  var a=10;
+  
+}
+
+console.log(a);
+
+
